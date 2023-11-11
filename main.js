8f65ff394467f9b4e7fa1b35fea4fe96a43e7ff7
Konstantin Alpatov <kraken2000abvgd@mail.ru>
2023-11-11 16:18:42 +0300

    an update

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// let pass = 'AkV';
+// let writePass = prompt('vvedite parol')
+
+// if (pass == writePass) {
+//     alert('Верно')
+//     } else {
+//   alert('неверно')
+// }
+//задание 2
+let c = 13
+if( c > 0 && c < 10)
+{
+console.log('верно')
+}
+else{
+  console.log('неверно')
+}
+//задание 3
+let d = 45
+let e = 1078
+if( d > 100 || e > 100)
+{
+console.log('верно')
+}
+else{
+  console.log('неверно')
+}
+//задание 4
+function socopros () {
+  let age = prompt('введите возраст')
+  if (age == 0 || age < 0) {
+    alert('неверное число')
+  } else if(age > 0 && age < 12) {
+    alert('ты крутыш')
+  }else if( age == 18 || age > 18)
+  {
+    alert('ничего себе')
+  }
+}
